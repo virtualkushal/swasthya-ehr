e8ba8f155bd5cf26a273b4b65b5daa6fcf6a950a
adityabajra <adityabajracharya18@gmail.com>
2026-07-09 21:03:44 +0545

    db pw

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("DB_NAME", "swasthya"),
         "USER": os.getenv("DB_USER", "postgres"),
-        "PASSWORD": os.getenv("DB_PASSWORD", "20610826"),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
         "HOST": os.getenv("DB_HOST", "localhost"),
         "PORT": os.getenv("DB_PORT", "5432"),
     }
